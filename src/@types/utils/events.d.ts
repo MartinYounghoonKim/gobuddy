eac618e6a74edfc586a3a1fb8060d0613d8c5304
martin <martin@tmon.co.kr>
2019-06-13 21:24:48 +0900

    Add utility's interface

---
+++ src/@types/utils/events.d.ts
@@ -0,0 +1,4 @@
+import * as React from "react";
+
+export type IHTMLElementChangeEvent = React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>;
+export type IHTMLSelectElementChangeEvent = React.ChangeEvent<HTMLSelectElement>
