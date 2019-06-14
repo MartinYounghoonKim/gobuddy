ec0159028db538beb7cb9d5e18133576c75b2088
martin <martin@tmon.co.kr>
2019-06-14 14:05:15 +0900

    Add webpack alias

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -40,9 +40,6 @@ const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
 
-function resolve (dir) {
-  return path.join(__dirname, '..', dir)
-}
 // This is the production and development configuration.
 // It is focused on developer experience, fast rebuilds, and a minimal bundle.
 module.exports = function(webpackEnv) {
@@ -268,7 +265,7 @@ module.exports = function(webpackEnv) {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
-        '@': resolve('src')
+        '@': path.resolve(__dirname, 'src')
       },
       plugins: [
         // Adds support for installing with Plug'n'Play, leading to faster installs and adding
