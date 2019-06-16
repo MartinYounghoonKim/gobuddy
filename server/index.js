a4b730b8f7936b13104eaf4b56ce2183e3d44d1b
martinyounghoonkim <hoons0131@gmail.com>
2019-06-16 11:05:07 +0900

    Seperate backend server and client

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', function (req, res, next) {
+  res.send('<h1>Hello React Server-Side Rendering</h1>');
+});
+
+app.listen(port, () => {
+  console.log('http://localhost:3000')
+});
