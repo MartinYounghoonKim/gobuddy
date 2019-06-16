ade44c1d85cc0f3acf0d0bdfd35aefff5d8b35a4
martinyounghoonkim <hoons0131@gmail.com>
2019-06-16 22:54:39 +0900

    Apply server side rendering, but something's wrong in babel-node.

--- server/index.js
+++ server/index.js
@@ -2,11 +2,30 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
+const ReactDomServer = require('react-dom/server');
+const Html = require("./Html");
+const App = require("../client/src");
 
 app.get('/', function (req, res, next) {
-  res.send('<h1>Hello React Server-Side Rendering</h1>');
+  let preloadState = {
+    text: 'Server-Side Rendering'
+  };
+
+  let renderProps = {
+    preloadState: `window.__PRELOADED_STATE__ =${JSON.stringify(preloadState).replace(/</g, '\\u003c')}`,
+    script: '/build/client.bundle.js'
+  };
+
+  ReactDomServer.renderToNodeStream(
+    <Html {...renderProps}>
+      <App data={preloadState}/>
+    </Html>
+  ).pipe(res);
+  // res.send('<h1>Hello React Server-Side Rendering</h1>');
 });
 
 app.listen(port, () => {
-  console.log('http://localhost:3000')
+  // const host = app.address().address;
+  // const port = app.address().port;
+  console.log('Express server has started on port:  http://localhost:3000')
 });
