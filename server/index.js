c68762c889734fd5b670272981bd7d5754bc7fc7
martinyounghoonkim <hoons0131@gmail.com>
2019-06-17 22:50:56 +0900

    Change commonjs syntax to es6

--- server/index.js
+++ server/index.js
@@ -1,27 +1,26 @@
-const express = require('express');
+import express from 'express';
+import React from 'react';
+import ReactDomServer from 'react-dom/server';
+import Html from "./Html";
 
 const app = express();
 const port = 3000;
-const ReactDomServer = require('react-dom/server');
-const Html = require("./Html");
-const App = require("../client/src");
+
+// const App = require("../client/src");
 
 app.get('/', function (req, res, next) {
-  let preloadState = {
-    text: 'Server-Side Rendering'
-  };
-
-  let renderProps = {
-    preloadState: `window.__PRELOADED_STATE__ =${JSON.stringify(preloadState).replace(/</g, '\\u003c')}`,
-    script: '/build/client.bundle.js'
-  };
-
+  // let preloadState = {
+  //   text: 'Server-Side Rendering'
+  // };
+  //
+  // let renderProps = {
+  //   preloadState: `window.__PRELOADED_STATE__ =${JSON.stringify(preloadState).replace(/</g, '\\u003c')}`,
+  //   script: '/build/client.bundle.js'
+  // };
+  //
   ReactDomServer.renderToNodeStream(
-    <Html {...renderProps}>
-      <App data={preloadState}/>
-    </Html>
+    <Html/>
   ).pipe(res);
-  // res.send('<h1>Hello React Server-Side Rendering</h1>');
 });
 
 app.listen(port, () => {
