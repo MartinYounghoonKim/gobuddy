de177aa217f44ddae51a1906bcf4741666994a54
martinyounghoonkim <hoons0131@gmail.com>
2019-06-24 20:37:45 +0900

    Add staticRouter in server side

--- server/index.js
+++ server/index.js
@@ -1,18 +1,20 @@
 import express from 'express';
 import React from 'react';
 import ReactDomServer from 'react-dom/server';
+import { StaticRouter, Route, Switch, NavLink } from "react-router-dom";
 import Html from "./Html";
-import App from "../client/src/App";
+// import Signup from "../client/src/pages/Signup.tsx";
+// import App from "../client/src/App";
 
 const app = express();
 const port = 3000;
 
 // const App = require("../client/src");
 
-app.get('/', function (req, res, next) {
-  // let preloadState = {
-  //   text: 'Server-Side Rendering'
-  // };
+app.get('/*', function (req, res, next) {
+  let preloadState = {
+    text: 'Server-Side Rendering'
+  };
   //
   const renderProps = {
     preloadState: `window.__PRELOADED_STATE__ =${JSON.stringify(preloadState).replace(/</g, '\\u003c')}`,
@@ -21,7 +23,12 @@ app.get('/', function (req, res, next) {
   //
   ReactDomServer.renderToNodeStream(
     <Html {...renderProps}>
-      <App />
+      <StaticRouter location={ req.url }>
+        <Switch>
+          <Route exact={true} path="/signup" component={() => <div><NavLink to="signin">signin</NavLink></div>}/>
+          <Route exact={true} path="/signin" component={() => <div><NavLink to="signup">signup</NavLink></div>}/>
+        </Switch>
+      </StaticRouter>
     </Html>
   ).pipe(res);
 });
