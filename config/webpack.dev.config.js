e68219ce9ed132093ccba4accb6e640cca204a89
martin <martin@tmon.co.kr>
2019-06-27 23:19:57 +0900

    Update server's webpack config

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,7 +1,19 @@
 const webpack = require('webpack');
 const path = require('path');
+const merge = require('webpack-merge');
+const baseWebpackConfigFactory = require("./webpack.config");
+const baseWebpackConfig = baseWebpackConfigFactory('development');
+const isDevMode = true;
+const settings = {
+  distPath: path.join(__dirname, "dist"),
+  srcPath: path.join(__dirname, "src")
+};
 
-module.exports = {
+function srcPathExtend(subpath) {
+  return path.join(settings.srcPath, subpath)
+}
+
+const webpackConfig = {
   target: 'node',
   entry: './server/index.js',
   output: {
@@ -10,22 +22,62 @@ module.exports = {
     path: path.resolve(__dirname, '../dist'),
     publicPath: '/',
   },
-  // Enable sourcemaps for debugging webpack's output.
   resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js", ".json"],
+    extensions: [".ts", ".tsx", ".js"],
   },
   module: {
     rules: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
       {
         test: /\.tsx?$/,
+        use: ["babel-loader", "ts-loader"]
+      },
+      {
+        test: /\.scss$/,
         use: [
+          "style-loader",
           {
-            loader: 'ts-loader',
+            loader: "css-loader",
+            options: {
+              sourceMap: isDevMode
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: [
+                require("autoprefixer")()
+              ],
+              sourceMap: isDevMode
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevMode
+            }
           }
         ]
       },
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "fonts/[name].[ext]",
+          },
+        },
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|ico)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "assets/"
+            }
+          }
+        ]
+      }
     ]
   },
   node: {
--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
-// import React from 'react';
-// import ReactDomServer from 'react-dom/server';
+import React from 'react';
+import ReactDomServer from 'react-dom/server';
 // import { StaticRouter, Route, Switch, NavLink } from "react-router-dom";
 // import Html from "./Html";
 // import Signup from "../client/src/pages/Signup.tsx";
 // import App from "../client/src/index.tsx";
+import Layout from "../client/src/Layout";
 
 const app = express();
 const port = 3002;
@@ -12,26 +13,42 @@ const port = 3002;
 // const App = require("../client/src");
 
 app.get('/*', function (req, res, next) {
-  // let preloadState = {
-  //   text: 'Server-Side Rendering'
-  // };
-  // //
-  // const renderProps = {
-  //   preloadState: `window.__PRELOADED_STATE__ =${JSON.stringify(preloadState).replace(/</g, '\\u003c')}`,
-  //   script: '/build/client.bundle.js'
-  // };
-  // const context = {};
-  // const html = ReactDomServer.renderToString(App);
-  // if (context.url) {
-  //   res.writeHead(302, {
-  //     Location: context.url
-  //   });
-  //   res.send();
-  // } else {
-  //   res.write(html);
-  //   res.send();
-  // }
-  res.send("test");
+  let preloadState = {
+    text: 'Server-Side Rendering'
+  };
+  //
+  const renderProps = {
+    preloadState: `window.__PRELOADED_STATE__ =${JSON.stringify(preloadState).replace(/</g, '\\u003c')}`,
+    script: '/build/client.bundle.js'
+  };
+  const context = {};
+  const html = ReactDomServer.renderToString(Layout);
+  console.log(html);
+  if (context.url) {
+    res.writeHead(302, {
+      Location: context.url
+    });
+    res.send();
+  } else {
+    res.send(`
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <title>TypeScript ReactJS SSR App</title>
+                    <style>
+                        body {
+                            margin: 0px;
+                            padding: 0px;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <main id="root">${html}</main>
+                    <script type="application/javascript" src="bundle.js"></script>
+                </body>
+            </html>
+        `);
+  }
 });
 
 app.listen(port, () => {
